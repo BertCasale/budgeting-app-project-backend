386e363f793b683cb8a7e330b686fc500da07726
BertCasale <bthebest55@gmail.com>
2023-04-17 19:56:57 -0400

    create model data

---
+++ models/budgetModel.js
@@ -0,0 +1,144 @@
+//Structure
+  // id - A unique number for each item
+    // item_name- string - the name of the transaction (ie: income, savings, cat food, etc.)
+    // amount -number - the amount of the transaction
+    // date- string - the date should be a simple string. As a bonus activity, use the date object and date input field and format it to be human-readable
+    // from - string - who this transaction was with (ie. employer, bank, pet store, grocery store, etc)
+    // category - string - what category does this fall into (income, savings, pets, food, etc)
+
+    // {
+    //   itemName: "",
+    //   amount: 0,
+    //   date: "yyyy-mm-dd",
+    //   from: "",
+    //   category: ""
+    // }
+
+module.exports = [
+  {
+    itemName: "Paycheck",
+    amount: 1000,
+    date: "2023-03-17",
+    from: "Work",
+    category: "Income"
+  },
+  {
+    itemName: "Hot Topic",
+    amount: -56,
+    date: "2023-03-18",
+    from: "Hot Topic",
+    category: "Clothing"
+  },
+  {
+    itemName: "F.Y.E.",
+    amount: -18,
+    date: "2023-03-18",
+    from: "F.Y.E.",
+    category: "Clothing"
+  },
+  {
+    itemName: "Subway Sandwich",
+    amount: -12,
+    date: "2023-03-18",
+    from: "Subway",
+    category: "Food"
+  },
+  {
+    itemName: "Game",
+    amount: -60,
+    date: "2023-03-18",
+    from: "Gamestop",
+    category: "Entertainment"
+  },
+  {
+    itemName: "Groceries",
+    amount: -253,
+    date: "2023-03-20",
+    from: "Stop & Shop",
+    category: "Food"
+  },
+  {
+    itemName: "Paycheck",
+    amount: 1000,
+    date: "2023-03-24",
+    from: "Work",
+    category: "Income"
+  },
+  {
+    itemName: "Burgers",
+    amount: -26,
+    date: "2023-03-24",
+    from: "Burger King",
+    category: "Food"
+  },
+  {
+    itemName: "Paycheck",
+    amount: 1000,
+    date: "2023-03-31",
+    from: "Work",
+    category: "Income"
+  },
+  {
+    itemName: "Car Payment",
+    amount: 450,
+    date: "2023-03-31",
+    from: "Bank",
+    category: "Loan Payment"
+  },
+  {
+    itemName: "Anime Subscription",
+    amount: -5,
+    date: "2023-04-01",
+    from: "Crunchyroll",
+    category: "Entertainment"
+  },
+  {
+    itemName: "Phone Bill",
+    amount: -65,
+    date: "2023-04-01",
+    from: "Verizon",
+    category: "Utilities"
+  },
+  {
+    itemName: "Groceries",
+    amount: -273,
+    date: "2023-04-05",
+    from: "Stop & Shop",
+    category: "Food"
+  },
+  {
+    itemName: "Paycheck",
+    amount: 1000,
+    date: "2023-04-07",
+    from: "Work",
+    category: "Income"
+  },
+  {
+    itemName: "Rent",
+    amount: -900,
+    date: "2023-04-10",
+    from: "Work",
+    category: "Rent"
+  },
+  {
+    itemName: "Electric Bill",
+    amount: -150,
+    date: "2023-04-11",
+    from: "Eversource",
+    category: "Utilities"
+  },
+  {
+    itemName: "Water Bill",
+    amount: -98,
+    date: "2023-04-11",
+    from: "CT Water",
+    category: "Utilities"
+  },
+  {
+    itemName: "Paycheck",
+    amount: 1000,
+    date: "2023-04-14",
+    from: "Work",
+    category: "Income"
+  }
+]   
