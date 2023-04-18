a6254a56acc6e4d27a5764bff432f061636aa390
BertCasale <bthebest55@gmail.com>
2023-04-17 20:24:40 -0400

    home and error page messages

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the Budget App");
+});
 
+app.get("*", (req, res) => {
+  res.status(404).json({success: false, error: "The page you are looking for cannot be found"});
+});
 
 module.exports = app;
