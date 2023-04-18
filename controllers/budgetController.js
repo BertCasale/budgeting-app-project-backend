5e3cd32c2622be476be721713768bdd3ca40e0fd
BertCasale <bthebest55@gmail.com>
2023-04-17 20:59:18 -0400

    update an existing budget item

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -23,4 +23,16 @@ budget.post("/", (req, res) => {
   res.status(203).json({success: true, data: budgetArray[budgetArray.length-1]});
 });
 
+//PUT/UPDATE
+budget.put("/:index", (req,res) => {
+  const {index} = req.params;
+
+  if (budgetArray[index]) {
+    budgetArray[index] = req.body;
+    res.status(203).json({success: true, data: budgetArray[index]});
+  } else {
+    res.status(404).json({succes: false, error: "The page you are looking for cannot be found or does not exist."});
+  }
+});
+
 module.exports = budget;
